4076a644dafebe5e9a4e88db415655e1d557f4ed
syedalim1 <syedsyed3777@gmail.com>
2025-01-10 17:45:48 +0530

    Front-end 35% Finish last added Caritem fINISH

--- src/CarItem.jsx
+++ src/CarItem.jsx
@@ -1,18 +1,44 @@
-import React from 'react'
-import { Separator } from './components/ui/separator';
-const CarItem = ({car}) => {
+import React from "react";
+import { Separator } from "./components/ui/separator";
+import { LuFuel } from "react-icons/lu";
+import { TbBrandSpeedtest } from "react-icons/tb";
+import { GiGearStickPattern } from "react-icons/gi";
+
+const CarItem = ({ car }) => {
   return (
     <div>
-      
-            <img src={car?.image} alt="" width={300} height={250} className='rounded-t-xl' />
-            <div>
-              <h2 className="font-bold text-black text-lg mb-2">{car?.name}</h2>
-            
-            </div>
-            <Separator className='bg-black'/>
-         
+      <img
+        src={car?.image}
+        alt=""
+        width={300}
+        height={250}
+        className="rounded-t-xl"
+      />
+      <div>
+        <h2 className="font-bold text-black text-lg mb-2">{car?.name}</h2>
+      </div>
+      <Separator className="bg-black" />
+      <div className="grid grid-cols-3 mt-5">
+        <div className="flex flex-col items-center">
+          <LuFuel className="text-lg mb-2"/>
+          <h2>{car.miles} Miles</h2>
+        </div>
+        <div className="flex flex-col items-center">
+          <TbBrandSpeedtest className="text-lg mb-2"/>
+          <h2>{car.fuelType} </h2>
+        </div>
+        <div className="flex flex-col items-center">
+          <GiGearStickPattern className="text-lg mb-2"/>
+          <h2>{car.gearType} </h2>
+        </div>
+      </div>
+      <Separator className='my-2 bg-black'/>
+      <div className="flex items-center justify-between">
+        <h2 className="font-bold text-xl">$ {car.price}</h2>
+        <h2 className="text-purple-500 text-sm"> View Details</h2>
+      </div>
     </div>
   );
-}
+};
 
-export default CarItem
+export default CarItem;
